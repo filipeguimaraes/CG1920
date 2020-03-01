36d1332a7b505d46ab9c22b2ec2bdf56be177a52
TheCanT <gonbcalo.nuno.ferreira.1999@gmail.com>
2020-03-01 20:37:48 +0000

    generator cone

--- generator/main.cpp
+++ generator/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <cstring>
+#include <math.h>
+
+#define REGULAR 1
+#define INVERT -(REGULAR)
 
 void write_point(FILE * f, float x, float y, float z){
     fprintf(f,"%f, %f, %f\n",x,y,z);
@@ -64,13 +68,13 @@ int box(char * file_path, float x, float y, float z, int div){
     //plano de cima
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeXZ(f,-x/2+(a*i),y/2,-z/2+(c*j),a,c,1);
+            planeXZ(f,-x/2+(a*i),y/2,-z/2+(c*j),a,c,REGULAR);
         }
     }
     //plano de baixo
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeXZ(f,-x/2+(a*i),-y/2,-z/2+(c*j),a,c,-1);
+            planeXZ(f,-x/2+(a*i),-y/2,-z/2+(c*j),a,c,INVERT);
         }
     }
 
@@ -78,13 +82,13 @@ int box(char * file_path, float x, float y, float z, int div){
     //plano da frente
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeXY(f,-x/2+(a*i),-y/2+(b*j),z/2,a,b,1);
+            planeXY(f,-x/2+(a*i),-y/2+(b*j),z/2,a,b,REGULAR);
         }
     }
     //plano de trás
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeXY(f,-x/2+(a*i),-y/2+(b*j),-z/2,a,b,-1);
+            planeXY(f,-x/2+(a*i),-y/2+(b*j),-z/2,a,b,INVERT);
         }
     }
 
@@ -92,13 +96,13 @@ int box(char * file_path, float x, float y, float z, int div){
     //plano de direita
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeYZ(f,x/2,-y/2+(b*j),-z/2+(c*i),b,c,1);
+            planeYZ(f,x/2,-y/2+(b*j),-z/2+(c*i),b,c,REGULAR);
         }
     }
     //plano da esquerda
     for (int i = 0; i < div; ++i) {
         for (int j = 0; j < div; ++j) {
-            planeYZ(f,-x/2,-y/2+(b*j),-z/2+(c*i),b,c,-1);
+            planeYZ(f,-x/2,-y/2+(b*j),-z/2+(c*i),b,c,INVERT);
         }
     }
 
@@ -106,13 +110,60 @@ int box(char * file_path, float x, float y, float z, int div){
     return 0;
 }
 
+void cone(char * file, float h, float r, float slices, float stacks){
+    FILE * f = fopen(file,"w");
+    float a = 0;
+    float h1 = 0;
+    float h2;
+    float r1 = r;
+    float r2 = 0;
+    float angulo = (float)(2 * M_PI) / (slices+1);
+
+
+    //base
+    while (a < 2 * M_PI) {
+        write_point(f,0,0, 0);
+        write_point(f,r * cos(a), 0, r * sin(a));
+        a += angulo;
+        write_point(f,r * cos(a), 0, r * sin(a));
+    }
+    a=0;
+
+    //h-((r/h)*x)
+    for (int i = 0; i < stacks; i++) {
+        r2 = r1 - r / stacks;
+        h2 =  h1 + h/stacks;
+        while (a < 2 * M_PI) {
+            write_point(f,r1 * cos(a), h1, r1 * sin(a));
+            write_point(f,r2 * cos(a), h2, r2 * sin(a));
+            write_point(f,r1 * cos(a+ angulo), h1, r1 * sin(a+angulo));
+
+            write_point(f,r2 * cos(a), h2, r2 * sin(a));
+            write_point(f,r2 * cos(a+angulo), h2, r2 * sin(a+angulo));
+            write_point(f,r1 * cos(a+angulo), h1, r1 * sin(a+angulo));
+            a += angulo;
+        }
+        h1 = h2;
+        r1 = r2;
+        a = 0;
+    }
+    fclose(f);
+}
+
 
 int main(int argc, char ** argv) {
     if(argc > 3 && !strcmp(argv[1],"plane") && isNumber(argv[2])){
         plane(argv[3],atof(argv[2]));
     }
-    else if(argc > 6 && !strcmp(argv[1],"box") && isNumber(argv[2]) && isNumber(argv[3]) && isNumber(argv[4])){
-        box(argv[6],atof(argv[2]),atof(argv[3]),atof(argv[4]),atoi(argv[5]));
+    else if(argc > 6 && !strcmp(argv[1],"box") && isNumber(argv[2]) && isNumber(argv[3])
+            && isNumber(argv[4]) && isNumber(argv[5])){
+        box(argv[6],atof(argv[2]),atof(argv[3])
+                ,atof(argv[4]),atoi(argv[5]));
+    }
+    else if(argc > 6 && !strcmp(argv[1],"cone") && isNumber(argv[2]) && isNumber(argv[3])
+            && isNumber(argv[4]) && isNumber(argv[5])){
+        cone(argv[6],atof(argv[2]),atof(argv[3])
+                ,atof(argv[4]),atof(argv[5]));
     }
     else puts("Invalid arguments.");
     return 0;
